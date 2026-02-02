a395fb63617789094b70445ac5db2c2eacd89b2a
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2026-02-02 21:00:46 +0000

    Update 06user_add_details.sql
    Ported changes from Private Branch

--- init/06user_add_details.sql
+++ init/06user_add_details.sql
@@ -1,8 +1,8 @@
 -- This Function will insert details once it passes checks from the stored procedure its called
 
-CREATE OR REPLACE FUNCTION add_patient_details(EFirstName VARCHAR(50), 
+CREATE OR REPLACE FUNCTION add_patient_details(EFirstName VARCHAR(50),
 ELastName VARCHAR(50),
-EGENDER VARCHAR(10), 
+EGENDER VARCHAR(10),
 EEmail VARCHAR(50),
 EPassword VARCHAR(255)
 )
@@ -20,18 +20,20 @@ return_msg int;
 
 BEGIN
 
-INSERT INTO accounts.users_accounts (email, password_hash)
-VALUES (EEmail, EPassword)
-  
 -- Insert into patient table
 
 INSERT INTO patients.patients (first_name, last_name, gender, email)
 VALUES (EFirstName, ELastName, EGender, EEmail)
 RETURNING user_id INTO return_msg;
 
+-- Insert into accounts table
+
+INSERT INTO accounts.users_accounts (user_id,email, password_hash)
+VALUES (return_msg, EEmail, EPassword);
+
 -- Return a message
 
-RETURN return_msg; 
+RETURN return_msg;
 
 END;
 $$;
